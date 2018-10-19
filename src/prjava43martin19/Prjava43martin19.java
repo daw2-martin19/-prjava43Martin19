d71017adf1f1c9a188326819ebe03b559af4b2a4
daw2-martin19 <javimartin@vivaldi.net>
2018-10-19 19:43:59 +0200

    Versio 0.6-C del proyecte prjava43martin19.Branca00 Modificada

--- src/prjava43martin19/Prjava43martin19.java
+++ src/prjava43martin19/Prjava43martin19.java
@@ -1,7 +1,8 @@
 package prjava43martin19; 
 import java.awt.*;        // using AWT containers and components 
         import java.awt.event.*;  // using AWT events and listener interfaces 
-        import java.io.*;
+        import java.text.*;
+import java.io.*;
  
 // An AWT GUI program inherits the top-level container java.awt.Frame 
 public class Prjava43martin19 extends Frame 
